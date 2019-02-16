9b9cf19fe1f0cd6fdf02b2632116de6ab99c184e
kt358 <kcslc@hotmail.com>
2019-02-16 02:08:43 +0200

    fixed script to include share

--- data/migration_json.py
+++ data/migration_json.py
@@ -1,15 +1,17 @@
-import pandas
+import pandas as pd
 import os
 import json
 
 csv_input = 'unhcr_time_series_normalized.csv'
-csv_output = 'migrate.json'
-migration_filter = 'Refugees (incl. refugee-like situations)'
+csv_output = '../Visualization/src/data/migrate.json'
 
-csv = pandas.read_csv(csv_input)
+# we can discuss if we want to use a slider for "type" to include IDP etc
+migration_filter = 'Refugees (incl. refugee-like situations)' 
+
+csv = pd.read_csv(csv_input)
 
 data = [
-    row[['year', 'FROM', 'TO', 'value']].to_json()
+    row[['year', 'iso-origin', 'iso-destination', 'value', 'share']].to_json()
     for index, row in csv.iterrows()
     if row['type'] == migration_filter
 ]
