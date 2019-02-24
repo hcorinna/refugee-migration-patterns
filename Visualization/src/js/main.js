fc4767ecebc8ab24c0cf64a0540bf9dce948135e
hcorinna <12029382+hcorinna@users.noreply.github.com>
2019-02-24 05:15:08 +0200

    Plot: HDI - # refugees

--- Visualization/src/js/main.js
+++ Visualization/src/js/main.js
@@ -5,6 +5,7 @@ var threshold;
 var path;
 var color;
 var svg;
+var features;
 var tooltip;
 var world;
 var indices;
@@ -48,7 +49,7 @@ $(function() {
 
   //colors for index metrics
   color = d3.scaleThreshold()
-      .domain(["0", "0,2", "0,3", "0,4", "0,5", "0,6", "0,7", "0,8", "0,9", "1"])
+      .domain([0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
       .range(["#a29bfe", "#e0ecf4", "#bfd3e6", "#9ebcda", "#8c96c6", "#8c6bb1", "#88419d", "#810f7c", "#4d004b"]);
 
   tooltip = d3.select("#map")
@@ -58,17 +59,19 @@ $(function() {
   // loading json stuffs
   d3.queue()
     .defer(d3.json, "src/data/50m.json")
-    .defer(d3.json, "src/data/indices.json")
+    .defer(d3.json, "src/data/country_features.json")
     .defer(d3.json,"src/data/migrate.json")
-    .await(function (error, world_data, indices_data, migration_data) {
+    .await(function (error, world_data, country_features, migration_data) {
         if (error) {
             console.error('Oh dear, something went wrong: ' + error);
         }
         else {
           world = world_data;
-          indices = indices_data;
+          indices = country_features;
           migration = migration_data;
+          createFeatures();
           drawMap();
+          drawHDIRefugeesPlot();
         }
     });
 
@@ -91,9 +94,7 @@ $(function() {
     d3.select("#selected-threshold").html(threshold);
 
     // Analyses
-    MARGIN = {top: 20, right: 20, bottom: 20, left: 20};
-
-    drawHDIRefugeesPlot();
+    MARGIN = {top: 20, right: 20, bottom: 30, left: 70};
 
 });
 
@@ -103,22 +104,43 @@ function updateMap() {
   drawMap();
 }
 
-function drawMap() {
-    var features = topojson.feature(world, world.objects.countries).features;
-    indicesByYear = {2006: {}, 2007: {}, 2008: {}, 2009: {}, 2010: {}, 2011: {}, 2012: {}, 2013: {}, 2014: {}, 2015: {}, 2016: {}};
+function createFeatures() {
+  features = topojson.feature(world, world.objects.countries).features;
+  indicesByYear = {2006: {}, 2007: {}, 2008: {}, 2009: {}, 2010: {}, 2011: {}, 2012: {}, 2013: {}, 2014: {}, 2015: {}, 2016: {}, 2017: {}};
 
-    indices.forEach(function (d) {
-      indicesByYear[d.year][d.iso] = {
-          hdi_value: d['hdi_value'],
-          hdi_rank: d['hdi_rank'],
-          fgi_value: d['fgi_value'],
-          fgi_rank: d['fgi_rank'],
-          hfi_rank: d['hfi_rank']
-      }
-    });
-    features.forEach(function (d) {
-        d.details = indicesByYear[year][d.id] ? indicesByYear[year][d.id] : {};
-    });
+  indices.forEach(function (d) {
+    indicesByYear[d.year][d.iso] = {
+        hdi_value: d['hdi_value'],
+        hdi_rank: d['hdi_rank'],
+        fgi_value: d['fgi_value'],
+        fgi_rank: d['fgi_rank'],
+        hfi_value: d['hfi_value'],
+        hfi_rank: d['hfi_rank'],
+        population: d['population'],
+        clustering_coeff: d['clustering_coeff'],
+        sorted_tri: d['sorted_tri'],
+        indegree_centrality: d['indegree_centrality'],
+        outdegree_centrality: d['outdegree_centrality'],
+        closeness_centrality: d['closeness_centrality'],
+        out_closeness_centrality: d['out_closeness_centrality'],
+        betweenness_centrality: d['betweenness_centrality'],
+        eigenvector_centrality: d['eigenvector_centrality'],
+        eigenvector_centrality_out: d['eigenvector_centrality_out'],
+        index: d['index'],
+        nxc: d['nxc'],
+        outflow: d['outflow'],
+        inflow: d['inflow'],
+        share: d['share'],
+        asylum_outflow: d['asylum_outflow'],
+        asylum_inflow: d['asylum_inflow']
+    }
+  });
+  features.forEach(function (d) {
+      d.details = indicesByYear[year][d.id] ? indicesByYear[year][d.id] : {};
+  });
+}
+
+function drawMap() {
     migration_features = migrate(features, migration, year);
 
     map = svg.append("g");
@@ -238,10 +260,10 @@ function migrate(world_data, migration_data, select_year){
         all_coordinates = longest_coordinates(d.geometry.coordinates);
         middle_pos = middle_coordinates(all_coordinates);
         d['pos'] = {
-        x:middle_pos.xy[0],
-        y:middle_pos.xy[1],
-        long:middle_pos.longlat[0],
-        lat:middle_pos.longlat[1]
+          x:middle_pos.xy[0],
+          y:middle_pos.xy[1],
+          long:middle_pos.longlat[0],
+          lat:middle_pos.longlat[1]
         };
         country_data.push(
         {
@@ -366,10 +388,14 @@ function drawHDIRefugeesPlot() {
       .ticks(5);
   var yAxis = d3.axisLeft(yScale)
       .ticks(5);
+  var xValue = function(d) { return d.details['hdi_value'];};
+  var yValue = function(d) { return d.details['outflow'];};
+  var xMap = function(d) { return xScale(xValue(d));};
+  var yMap = function(d) { return yScale(yValue(d));};
 
   // x-axis
   hdiRefugees.append("g")
-      .attr("class", "x axis")
+      .attr("class", "x axis-line")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
     .append("text")
@@ -381,7 +407,7 @@ function drawHDIRefugeesPlot() {
 
   // y-axis
   hdiRefugees.append("g")
-      .attr("class", "y axis")
+      .attr("class", "y axis-line")
       .call(yAxis)
     .append("text")
       .attr("class", "y axis-label")
@@ -390,4 +416,36 @@ function drawHDIRefugeesPlot() {
       .attr("dy", ".71em")
       .style("text-anchor", "end")
       .text("Number of Refugees");
+
+  var tooltipDot = d3.select("body").append("div")
+    .attr("class", "tooltip-dot")
+    .style("opacity", 0);
+
+  hdiRefugees.selectAll(".dot")
+      .data(features)
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", xMap)
+      .attr("cy", yMap)
+      .style("fill", "#fff")
+      .on("mouseover", function(d) {
+        d3.select(this)
+            .style("stroke", "#c0392b")
+            .style("cursor", "pointer");
+        tooltipDot.transition()
+             .duration(200)
+             .style("opacity", .9);
+        tooltipDot.html(d.properties.name + "<br/> (" + xValue(d)
+          + ", " + yValue(d) + ")")
+             .style("left", (d3.event.pageX + 5) + "px")
+             .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+        d3.select(this)
+            .style("stroke", "#e67e22");
+        tooltipDot.transition()
+             .duration(500)
+             .style("opacity", 0);
+      });
 }
